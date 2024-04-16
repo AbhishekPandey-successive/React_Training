75a400c3e89c4a34879bb0abf58bef3fd7ec9ca5
Abhishek Pandey <abhishek.pandey@successive.tech>
2024-04-16 16:34:38 +0530

    updated Question4

--- react-app/src/components/Day4/InputFieldComponent.jsx
+++ react-app/src/components/Day4/InputFieldComponent.jsx
@@ -1,34 +1,21 @@
 // 3.Create a component with controlled input field and a button. When the user enters a specific value into the input (e.g., "show"), a new component should be rendered below the input, displaying a message. Otherwise, nothing should be displayed.
 
 
-import React,{useState} from 'react'
-
-
+import React, { useState } from 'react'
 
 function InputFieldComponent() {
-    const [text,setText]=useState("")
-    const [showmessage,setShowMessage]=useState(false)
-    const [component,setComponent]=useState("")
+  const [text,setText]=useState("")
+  const[component,setComponent]=useState("")
 
-   function checkMessage()
-   {
-    if(showmessage)
-    {
-        setShowMessage(false)
-    }
-    else{
-        setShowMessage(true)
-    }
-
+  function takeText()
+  {
     setComponent(text)
-    
-   }
+  }
   return (
     <div>
-      <input type='text' value={text} onChange={(e)=>setText(e.target.value) }/>
-      <button onClick={checkMessage}> Send Message to new Component</button>
-      <br/>
-      {showmessage && component}
+   <input type='text' value={text} onChange={(e)=>setText(e.target.value)}/>
+   <button onClick={takeText}>Take the Text</button>
+   {component && <h1>{component}</h1>}
       
     </div>
   )
